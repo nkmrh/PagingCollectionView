d15a243bd1e3b8b7c927bef8fa0c7126ff414975
nkmrh <nkmrhj@gmail.com>
2019-08-05 19:12:36 +0900

    Add Xcodeproj

---
+++ PagingCollectionView/FlowLayout.swift
@@ -0,0 +1,5 @@
+import UIKit
+
+final class FlowLayout: UICollectionViewFlowLayout {
+
+}
---
+++ PagingCollectionView/ViewController.swift
@@ -0,0 +1,48 @@
+import UIKit
+
+final class ViewController: UICollectionViewController {
+
+    private let cellAspectRatio: CGFloat = 4 / 3
+
+    private var cellSize: CGSize {
+        let width = collectionView.bounds.width * 0.8
+        let height = width * cellAspectRatio
+        return CGSize(width: width, height: height)
+    }
+
+    private var headerSize: CGSize {
+        let width = collectionView.bounds.width * 0.4
+        return CGSize(width: width, height: 0)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let flowLayout = collectionViewLayout as! FlowLayout
+        flowLayout.scrollDirection = .horizontal
+        flowLayout.itemSize = cellSize
+        flowLayout.minimumInteritemSpacing = collectionView.bounds.height
+        flowLayout.minimumLineSpacing = 20
+        flowLayout.sectionInset = UIEdgeInsets(top: 0, left: 40, bottom: 0, right: 40)
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        cell.layer.cornerRadius = 30
+        return cell
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        return collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "Header", for: indexPath)
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return headerSize
+    }
+}
